00b525950e184ad9fdcba17c425566fb421c54cd
xiaominglol <474345633@qq.com>
2019-05-17 18:10:58 +0800

    测试

---
+++ README.md
@@ -0,0 +1,27 @@
+framework   -----   框架工程
+
+cloud-eureka        服务注册中心
+
+cloud-config        配置中心
+
+cloud-config-repo   配置管理
+
+cloud-gateway       路由网关
+
+cloud-turbine       服务监控
+
+cloud-zipkin        服务跟踪
+
+------------------------------
+
+business    -----   业务工程
+
+cloud-web           Web工程
+
+cloud-stream-mq     RabbitMq工程
+
+cloud-redis         Redis工程
+
+cloud-es            ElasticSearch工程
+
+cloud-admin         Admin工程
---
+++ cloud-web/src/main/java/com/gemini/web/mvc/model/Message.java
@@ -0,0 +1,61 @@
+package com.gemini.web.mvc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 封装返回结果集
+ *
+ * @author 小明不读书
+ * @date 2017-10-10
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Message {
+
+    /**
+     * 状态，success成功，fail失败
+     */
+    private String status;
+
+    /**
+     * 提示信息
+     */
+    private String message;
+
+    /**
+     * 返回数据
+     */
+    private Object data;
+
+    /**
+     * 用户要返回给浏览器的数据
+     */
+    private Map<String, Object> result = new HashMap<String, Object>();
+
+    public static Message success(Object data) {
+        Message message = new Message();
+        message.setStatus("success");
+        message.setData(data);
+        return message;
+    }
+
+    public static Message fail(String msg) {
+        Message message = new Message();
+        message.setStatus("fail");
+        message.setMessage(msg);
+        return message;
+    }
+
+    public Message add(String key, Object value) {
+        this.getResult().put(key, value);
+        return this;
+    }
+}

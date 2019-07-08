b0604bcc906300df1340a6ee37083aba7850eeeb
xiaominglol <474345633@qq.com>
2019-07-08 10:39:30 +0800

    命名大整改

--- cloud-framework-example/cloud-mybatis-example/src/main/java/com/gemini/cloud/framework/mybatis/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.gemini.cloud.framework.redis.service;
-
-import com.gemini.cloud.framework.redis.moder.Member;
-
-import java.util.List;
-
-public interface MemberService {
-    List<Member> list();
-}
--- cloud-framework-example/cloud-redis-example/src/main/java/com/gemini/cloud/framework/redis/service/MemberService.java
+++
@@ -1,7 +0,0 @@
-package com.gemini.cloud.framework.redis.service;
-
-import java.util.List;
-
-public interface MemberService {
-    List<Object> list();
-}
